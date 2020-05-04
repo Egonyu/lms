1284751e39cb0ad67f7d054b11fe7ff08ab97127
hasefumi23 <hasefumi23@gmail.com>
2020-05-05 00:44:54 +0300

    New updates for the vulnerabilities

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-12-01 13:21:14.
+    * Generated on 2020-05-04 21:25:59.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.28 on 2019-12-01 13:21:13.
+ * Generated for Laravel 5.7.28 on 2020-05-04 21:25:58.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
